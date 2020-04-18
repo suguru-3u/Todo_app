ae8aef86a3c4867b951081d06bdc2c8474f277b6
suguru-3u <suguru.13579515@gmail.com>
2020-04-18 12:53:44 +0000

    [finish] Today

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root 'tops#home'
-  resources:users
   get '/login',to:'sessions#new'
   post '/login',to:'sessions#create'
   delete '/logout',to:'sessions#destroy'
+  resources:users
   resources:lists
+  resources :account_activations, only: [:edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_15_205637) do
+ActiveRecord::Schema.define(version: 2020_04_18_011522) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,9 @@ ActiveRecord::Schema.define(version: 2020_04_15_205637) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
